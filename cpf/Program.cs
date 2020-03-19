48456b7ffb0b709fbe144a52f2c9f61de96467ed
Leandro <leandrorochaadm@gmail.com>
2020-03-19 13:44:01 -0400

    concatenando e deixando somente numeros

---
+++ cpf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace cpf
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            string cpfEntrada = "";
+            foreach (string element in args) {
+                cpfEntrada += element;
+            }
+
+            string cpf = String.Join("", System.Text.RegularExpressions.Regex.Split(cpfEntrada, @"[^\d]"));
+            Clipboard.SetText(cpf);
+            /*
+            Console.WriteLine("Numero digitado foi: "+ cpfEntrada);
+            Console.WriteLine("Numero sem pontos: "+ cpf);
+            Console.ReadLine();
+            */
+
+        }
+    }
+}
